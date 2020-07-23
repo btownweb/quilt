39ca0152773de6636facce541b26241bdb2e02fa
Brian Ryu <brian.ryu@shopify.com>
2020-07-22 21:14:32 -0400

    Fix script

--- scripts/copy_cache_to_build.sh
+++ scripts/copy_cache_to_build.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-for p in .package-build-cache/*/build; do
-    mkdir -p packages/${p:21}
-    rm -rf packages/${p:21}/*
-    cp -a $p/* packages/${p:21}
-done
+if [ -d .package-build-cache ]
+then
+    for p in .package-build-cache/*/build; do
+        mkdir -p packages/${p:21}
+        rm -rf packages/${p:21}/*
+        cp -a $p/* packages/${p:21}
+    done
+fi
